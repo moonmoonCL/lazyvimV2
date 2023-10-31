f43dbde390d8681130034c48a47a32e63fdc3560
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-10-31 20:01:11 +0800

    feat: telescope horizontal config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
+  opts = {
+    defaults = {
+      layout_config = {
+        horizontal = {
+          width = 0.99,
+          height = 0.8,
+          prompt_position = "bottom",
+          preview_cutoff = 100,
+        },
+      },
+    },
+  },
+}
