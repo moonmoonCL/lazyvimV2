47829c9a5c7915cf6c27defa10794884fef99adc
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-10-31 23:46:25 +0800

    feat: add zenmode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- 保存本地变量
-local map = vim.api.nvim_set_keymap
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = Util.safe_keymap_set
 local opt = { noremap = true, silent = true }
 map("n", "Q", ":w<CR>", opt)
 
@@ -12,3 +17,5 @@ map("n", "<A-e>", ":NvimTreeFindFileToggle<CR>", opt)
 -- diffview
 -- map("n", "<A-d>o", ":DiffviewOpen<CR>", opt)
 -- map("n", "<A-d>c", ":DiffviewClose<CR>", opt)
+map("n", "<leader>uz", ":ZenMode<CR>", { desc = "ZenMode" })
+map("n", "<leader>qw", ":q<CR>", { desc = ":q" })
