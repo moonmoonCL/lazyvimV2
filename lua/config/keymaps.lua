ca7698c5561f497f560f7a6721c32ea0b3397e19
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-11-02 11:03:44 +0800

    fix: remove autosave

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,8 +21,8 @@ map("n", "<leader>uz", ":ZenMode<CR>", { desc = "ZenMode" })
 map("n", "<leader>qw", ":q<CR>", { desc = ":q" })
 
 -- harpoon
-map("n", "<leader>ja", ":lua require('harpoon.mark').add_file()<CR>", { desc = "harpoon add" })
-map("n", "<leader>jf", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = "harpoon find" })
+map("n", "<leader>fa", ":lua require('harpoon.mark').add_file()<CR>", { desc = "harpoon add" })
+map("n", "<leader>fj", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = "harpoon find" })
 
 -- autosave
-map("n", "<leader>ua", ":ASToggle<CR>", { desc = "toggle autosave" })
+-- map("n", "<leader>ua", ":ASToggle<CR>", { desc = "toggle autosave" })
