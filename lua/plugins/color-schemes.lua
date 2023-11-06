72576a1d6c5943cdeb9d20b4f220d0eedacb0f16
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-11-06 17:30:27 +0800

    color theme

--- lua/plugins/color-schemes.lua
+++ lua/plugins/color-schemes.lua
@@ -2,7 +2,18 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      -- colorscheme = "everforest",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      style = "moon",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
   {
